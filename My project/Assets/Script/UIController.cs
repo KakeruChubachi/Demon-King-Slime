3556a6392b0b46435ea270bb8619ba5b8c6b6d1f
ChubachiKakeru <sdt2595gp0012@edu.sca.ac.jp>
2026-07-23 22:50:09 +0900

    UI作成

---
+++ My project/Assets/Script/UIController.cs
@@ -0,0 +1,11 @@
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UIController : MonoBehaviour
+{
+    public Text slimeLabelText;
+    public Text lifeText;
+    public Text timeText;
+    public Text stageText;
+
+}
